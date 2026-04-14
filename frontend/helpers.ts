7a91ad4da10a5661fd1b7ac17a0ba959b4061833
matt <mneckelmann@gmail.com>
2026-04-14 21:59:30 +0200

    Fix present value table data

---
+++ frontend/helpers.ts
@@ -0,0 +1,7 @@
+export function calculateMultiplier(years: number, rate: number): number {
+    const i = rate / 100;
+    return (1 - Math.pow(1 + i, -years)) / i;
+  }
+  
+
+  console.log(calculateMultiplier(37, 1.5));
